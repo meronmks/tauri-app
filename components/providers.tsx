0ef23af25f097eba0765e97868d55a902b18067b
meronmks <meronmks.8914@gmail.com>
2024-04-12 19:32:49 +0900

    react-toastifyの表示が崩れていたのを修正

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,11 +5,20 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 
+import 'react-toastify/dist/ReactToastify.min.css';
+
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <>
             <ToastContainer
                 position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                draggable
+                theme="light"
             />
             <I18nextProvider i18n={i18next}>
                 <ThemeProvider value={{
