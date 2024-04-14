6734e9564df0dac97ceea5a558ccd1bad61a0e1e
meronmks <meronmks.8914@gmail.com>
2024-04-14 17:24:11 +0900

    socketの接続方法整理

--- components/tlcolumn.tsx
+++ components/tlcolumn.tsx
@@ -4,10 +4,13 @@ import React, { useEffect } from "react";
 import { Card, Avatar, List, ListItem } from "@material-tailwind/react";
 import TimelineNote from "./timelinenote";
 import WebSocket from "tauri-plugin-websocket-api";
+import { toastNormal } from "@/lib/toast";
 
-export function TLColumn({ className, endpoint }: {
+export function TLColumn({ className, endpoint, channel, token }: {
     className?: string;
     endpoint: string;
+    channel: string;
+    token?: string;
 }) {
     const wss = React.useRef<WebSocket | null>(null);
     const [notes, setNotes] = React.useState([] as any[]);
@@ -15,19 +18,26 @@ export function TLColumn({ className, endpoint }: {
     React.useEffect(() => {
         console.debug(`Loading TLColumn with endpoint ${endpoint}`);
         let unmounted = false;
+        let intervalTimeout: NodeJS.Timeout | null = null;
 
         const connectWebSocket = async () => {
             console.debug(`Connecting to WebSocket at ${endpoint}`);
-            wss.current = await WebSocket.connect(`wss://${endpoint}/streaming`);
+            if (token) {
+                wss.current = await WebSocket.connect(`wss://${endpoint}/streaming?i=${token}`);
+            } else {
+                wss.current = await WebSocket.connect(`wss://${endpoint}/streaming`);
+            }
+            
             if (unmounted) {
                 disconnectWebSocket();
                 return;
             }
-            console.debug(`Connected to WebSocket at ${endpoint}`);
+            toastNormal(`Connected to WebSocket at ${endpoint}`);
 
             wss.current.addListener((msg: any) => {
                 try {
                     if (msg.data){
+                        console.debug(msg.data);
                         const jsonData = JSON.parse(msg.data);
                         setNotes(prevNotes => [jsonData, ...prevNotes]);
                         console.debug(`Received WebSocket message!`);
@@ -40,7 +50,7 @@ export function TLColumn({ className, endpoint }: {
             await wss.current.send(JSON.stringify({
                 "type": "connect",
                 "body": {
-                    "channel": "localTimeline",
+                    "channel": channel,
                     "id": "1",
                     "params": {
                         "withRenotes": true,
@@ -49,7 +59,7 @@ export function TLColumn({ className, endpoint }: {
                 }
             }));
 
-            setInterval(() => {
+            intervalTimeout = setInterval(() => {
                 console.debug(`Sending keepalive to WebSocket ${endpoint}`);
                 wss.current?.send("h").catch((e) => {
                     console.error(`Error sending keepalive: ${e}`);
@@ -64,6 +74,10 @@ export function TLColumn({ className, endpoint }: {
                 console.debug("Disconnecting from WebSocket");
                 wss.current.disconnect();
                 wss.current = null;
+            }
+            if (intervalTimeout) {
+                clearInterval(intervalTimeout);
+                intervalTimeout = null;
             }
         };
 
